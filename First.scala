6f53c859f515c57b5d3628dfa93bb454d3eedb24
W.K.S.Wishwanath <86224482+Wish245@users.noreply.github.com>
2023-06-15 20:19:33 +0530

    Add files via upload
    1. Consider the following variables
    i, j, m, n, k, f, g, c
    Declare the variables in Scala and assign them with the initial values as follows:
     k = i = j = 2;
     m = n = 5;
    f = 12.0f;
    g = 4.0f;
    c = ‘X’;
    Evaluate the following expressions:
    a) k + 12 * m
    b) m / j
    c) n % j
    d) m / j * j
    e) f + 10*5 +g
    f) ++i * n
    Answer to the above question

---
+++ First.scala
@@ -0,0 +1,17 @@
+object First {
+  def main(args: Array[String]): Unit ={
+    val (k:Int,j:Int)=(2,2)
+    val i:Int = 2
+    val m:Int = 5
+    val n:Int = 5
+    val g = 4.0
+    val f= 12.0
+    val c:Char = 'X'
+    println("k+12*m " ,k +12 * m)
+    println("m/j " ,m / j)
+    println("n%j ", n % j)
+    println("m/j*j ", m / j * j)
+    println("f+10*5+g ",f + 10 * 5 + g)
+    println("++i*n " ,(i+1)*n)
+  }
+}
