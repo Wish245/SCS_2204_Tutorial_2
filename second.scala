38dec12bc438d6f05c23f5734e5411eb6642a200
W.K.S.Wishwanath <86224482+Wish245@users.noreply.github.com>
2023-06-19 10:57:42 +0530

    Update second.scala

--- second.scala
+++ second.scala
@@ -1,12 +1,12 @@
 object second {
-  def main(args: Array[String]) = {
+  def main(args: Array[String]): Unit = {
     val a: Int = 2
     val b: Int = 3
     val c: Int = 4
     val d: Int = 5
     val k = 4.3
     val g = 0
-    println("--b*a+c*d-- =", (b-1)*a+c*d)
+    println("--b*a+c*d-- =", b*a+c*(d-1))
     println("a+1", a+1)
     println("-2*(g-k)+c",-2*(g-k)+c)
     println("c=c++",(c+1))
